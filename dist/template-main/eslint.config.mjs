7d5fe277c2ee9429e216c9c9c0afce11e6e30510
Vinayak Suthar <sutharvinayak28@gmail.com>
2025-03-19 15:19:22 +0530

    feat: add template basics

---
+++ dist/template-main/eslint.config.mjs
@@ -0,0 +1,42 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(), {
+    plugins: {},
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+            ...globals.node,
+        },
+
+        ecmaVersion: 11,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {},
+}];
